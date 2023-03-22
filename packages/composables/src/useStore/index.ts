e75da4e312026b914e48567d022a09d7b8917e32
askie <askie@qq.com>
2023-03-22 18:27:38 +0800

    feat: aa

--- packages/composables/src/useStore/index.ts
+++ packages/composables/src/useStore/index.ts
@@ -1,10 +1,11 @@
 import { Store } from '@vue-storefront/xbuy-api';
-import { Context, useStoreFactory } from '@vue-storefront/core';
+import { Context, useStoreFactory, useVSFContext } from '@vue-storefront/core';
 
 export const useStore = useStoreFactory<Store>({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   load(context: Context, params) {
-    console.log('Mocked: useStore.loa----------');
+    const a = useVSFContext();
+    console.log('Mocked: useStore.loa----------', a.req.headers);
     context.$xbuy.api.api1(params);
 
     return Promise.resolve({});
